0a03763fdad20d96b3d2228a8a04091fc48c908d
lluthfiamalia <lluthfiamalia14@gmail.com>
2026-05-28 16:55:31 +0700

    feat: fix loginPage, routes in app.jsx, create modal for forgot password

--- src/App.jsx
+++ src/App.jsx
@@ -6,34 +6,36 @@ import LoginPage from "./pages/LoginPage";
 import BoardPage from "./pages/BoardPage";
 import NotFoundPage from "./pages/NotFoundPage";
 import BoardDetailPage from "./pages/BoardDetail";
+import RegisterPage from "./pages/RegisterPage";
 
 function App() {
-	return (
-		<>
-			<Routes>
-				<Route element={<AuthLayout />}>
-					<Route path="/login" element={<LoginPage />} />
-				</Route>
+  return (
+    <>
+      <Routes>
+        <Route element={<AuthLayout />}>
+          <Route path="/login" element={<LoginPage />} />
+          <Route path="/register" element={<RegisterPage />} />
+        </Route>
 
-				<Route element={<MainLayout />}>
-					<Route path="/" element={<Navigate to="/dashboard" replace />} />
-					<Route path="/dashboard" element={<BoardPage />} />
-					<Route path="/dashboard/detail" element={<BoardDetailPage />} />
-				</Route>
+        <Route element={<MainLayout />}>
+          <Route path="/" element={<Navigate to="/dashboard" replace />} />
+          <Route path="/dashboard" element={<BoardPage />} />
+          <Route path="/dashboard/detail" element={<BoardDetailPage />} />
+        </Route>
 
-				<Route path="*" element={<NotFoundPage />} />
-			</Routes>
+        <Route path="*" element={<NotFoundPage />} />
+      </Routes>
 
-			<ToastContainer
-				position="bottom-left"
-				autoClose={2000}
-				hideProgressBar={false}
-				newestOnTop
-				closeOnClick
-				pauseOnHover
-			/>
-		</>
-	);
+      <ToastContainer
+        position="bottom-left"
+        autoClose={2000}
+        hideProgressBar={false}
+        newestOnTop
+        closeOnClick
+        pauseOnHover
+      />
+    </>
+  );
 }
 
 export default App;
